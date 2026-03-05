2cab892a5bca4b64b825d107c60e6426f9696610
KhAppAPT <63113556+KhAppAPT@users.noreply.github.com>
2026-03-05 15:03:19 +0000

    Refactor checker.py to use command line arguments

--- checker.py
+++ checker.py
@@ -1,31 +1,27 @@
+import sys
+
 bets = [
-    {"nums": [12,13,18,19,26], "stars": [4,11]},
-    {"nums": [21,30,33,45,50], "stars": [3,7]},
-    {"nums": [2,10,13,28,38], "stars": [2,11]},
-    {"nums": [17,20,28,41,44], "stars": [6,7]},
+    {"nums":[12,13,18,19,26],"stars":[4,11]},
+    {"nums":[21,30,33,45,50],"stars":[3,7]},
+    {"nums":[2,10,13,28,38],"stars":[2,11]},
+    {"nums":[17,20,28,41,44],"stars":[6,7]},
 ]
 
-print("Introduz os números sorteados:")
+if len(sys.argv) != 8:
+    print("Uso: python checker.py n1 n2 n3 n4 n5 e1 e2")
+    exit()
 
-numeros = list(map(int, input("5 números (separados por espaço): ").split()))
-estrelas = list(map(int, input("2 estrelas (separadas por espaço): ").split()))
+draw_numbers = list(map(int, sys.argv[1:6]))
+draw_stars = list(map(int, sys.argv[6:8]))
 
-print("\nResultado do sorteio:")
-print("Números:", numeros)
-print("Estrelas:", estrelas)
-
-print("\nResultados das apostas:\n")
+print("Números sorteados:", draw_numbers)
+print("Estrelas:", draw_stars)
 
 for i, bet in enumerate(bets, start=1):
 
-    acertos_nums = len(set(bet["nums"]) & set(numeros))
-    acertos_stars = len(set(bet["stars"]) & set(estrelas))
+    matched_numbers = len(set(bet["nums"]) & set(draw_numbers))
+    matched_stars = len(set(bet["stars"]) & set(draw_stars))
 
-    print(f"Aposta {i}")
-    print("Acertos números:", acertos_nums)
-    print("Acertos estrelas:", acertos_stars)
-
-    if acertos_nums >= 2:
-        print("👉 Possível prémio!")
-
-    print()
+    print(f"\nAposta {i}")
+    print("Números acertados:", matched_numbers)
+    print("Estrelas acertadas:", matched_stars)
